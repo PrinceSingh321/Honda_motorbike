6ccd415bd5ef10a2c69b66fb0c123836f6d19d58
Prince Singh <114475048+PrinceSingh321@users.noreply.github.com>
2024-12-10 13:13:51 +0530

    today cahnges

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,22 @@
-import LoginForm from '../models/signup';
-import 'bootstrap/dist/css/bootstrap.css';
+import Image from "next/image";
 
 export default function Home() {
-  
   return (
-    <>
-      <LoginForm />
-    </>
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+        <div className="col-md-4">
+          <label>Name</label>
+          <input type="name" name="name" placeholder="Enter Name"></input>
+        </div>
+
+        <div className="col-md-4">
+          <label>Last Name</label>
+          <input type="name" name="name" placeholder="Enter Last Name"></input>
+        </div>
+
+
+      </main>
+      
+    </div>
   );
 }
